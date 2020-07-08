c4d9cbaa3aa463d816a0a4756f612f733aa4e65b
Pradhyum R <drpradhyum2016@outlook.com>
2020-07-08 15:50:32 -0400

    Added Machine class

--- VM.py
+++ VM.py
@@ -9,6 +9,8 @@ class Function:
         assert(len(args) == self.arity)        # This wastes time; I'll remove it later
         return function(args)
 
+f = Function  # In essence, this is a typedef
+
 class EvalTree:
     def __init__(self, function, arity, branches=[], parent=None):
         self.function = function
@@ -31,10 +33,32 @@ class EvalTree:
 
 class Machine:
     instructionset = {
-       '+': operator.add
+       '+': f(operator.add, 2)
     }
     def __init__(self):
         self.stack = []
 
-    def run(self, instructions):
-        pass
+    def makeTree(self, instructions: list) -> EvalTree:
+        # 1. Scan the instructions for functions
+        def isfunction(code: str) -> bool:
+            return code in Machine.instructionset.keys()
+
+        function = map(isfunction, instructions)
+        
+        # 2. Recursivly construct the tree
+        args = []
+        zipped = zip(range(len(instructions)), instructions, function)
+        f = Machine.instructionset[instructions[0]]
+        for i, ins, isfunc in zipped:
+            if isfunc:
+                r = []
+                function = ins
+                i, ins, isfunc = next(zipped)
+                while len(r) <= Machine.instructionset[function].arity:
+                    r.append(inc)
+                args.append(None)
+            else:
+                args.append(ins)
+        
+        # 3. Run the tree
+        EvalTree(f, f.arity, args).eval()
