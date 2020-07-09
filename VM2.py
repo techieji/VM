7a232445ca62ad133e29b708850ae7e7636596f1
Pradhyum R <drpradhyum2016@outlook.com>
2020-07-09 16:18:22 -0400

    Broke, then fixed the VM. Macro names changed.

--- VM2.py
+++ VM2.py
@@ -1,6 +1,11 @@
 import operator
 import sys
 
+variables = {}
+
+def setvar(varname, value): # I have to do this because of Python's stupid lambda expressions
+    variables[varname] = value
+
 instructionset = {                                 # everything has to be in reverse order because the vm is stack-based
         "int": (1, lambda x: int(x)),
         "str": (1, lambda x: str(x)),
@@ -16,7 +21,9 @@ instructionset = {                                 # everything has to be in rev
         "eq": (2, lambda x, y: x == y),
         "lt": (2, lambda x, y: y < x),
         "gt": (2, lambda x, y: y > x),
-        "if": (3, lambda x, y, z: y if z else x)
+        "if": (3, lambda x, y, z: y if z else x),
+        "set": (2, lambda x, y: setvar(x, y)),
+        "get": (1, lambda x: variables[x])
 }
 
 class Machine:
@@ -52,27 +59,55 @@ def formattext(text: str):
     return ins
 
 def execute(text: str):
-    m = Machine()
-    ins = text.split('\n')
-    ins = list(filter(bool, ins))
+    ins = formattext(text)
     instructions = []
     macros = {}
     for x in ins:
-        if x[0] == '-':
-            if x[0:9] == '-include(':
-                filename = x[9:-1]
+        if x[0:2] == '--':
+            if x[0:9] == '--include':
+                filename = x[10:-1]
                 with open(filename) as f:
                     filetext = formattext(f.read())
-                    modulename = filetext[0][8:-1]
+                    modulename = filetext[0][11:-1]
                     macros[modulename] = filetext[1:]
-        elif x[0] != ';':
+        else:
             if x in macros.keys():
                 macrovalue = macros[x]
                 for i in macrovalue:
                     instructions.append(i)
             else:
                 instructions.append(x)
+    m = Machine()
     m.execute(instructions)
 
-with open(sys.argv[1]) as f:
-    execute(f.read())
+def execute2(ins: str):
+    m = Machine()
+    ins = formattext(ins)
+    m.execute(ins)
+
+def repl():
+    m = Machine()
+    try:
+        while True:
+            m.doins(input('> '))
+    except KeyboardInterrupt:
+        repl()
+    except EOFError:
+        return
+
+def main():
+    if len(sys.argv) == 2:
+        with open(sys.argv[1]) as f:
+            execute(f.read())
+    elif len(sys.argv) == 3:
+        with open(sys.argv[1]) as f:
+            if sys.argv[2] == "--nomacro":
+                execute2(f.read())
+            elif sys.argv[2] == "--macro":
+                execute(f.read())
+    else:
+        print("Entering REPL...")
+        repl()
+
+if __name__ == "__main__":
+    main()
